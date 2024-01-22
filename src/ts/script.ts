f6f9b218ecc04fa5be9c24e7b973b83f88296095
Brian Simiyu <briansimiyuj@gmail.com>
2024-01-22 17:36:33 +0300

    Select pic functionality

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,3 +1,5 @@
+import initializeSelectPic from './select.js'
+
 const desktopUploadLink = document.querySelector("#desktopUploadLink") as HTMLAnchorElement,
       mobileUploadLink = document.querySelector("#mobileUploadLink") as HTMLAnchorElement,
       fileInput = document.querySelector(".fileInput") as HTMLInputElement
@@ -7,10 +9,14 @@ desktopUploadLink.addEventListener("click", () =>{
 
     fileInput.click()
 
+    const selectPic = initializeSelectPic()
+
 })
 
 mobileUploadLink.addEventListener("click", () =>{
 
     fileInput.click()
+
+    const selectPic = initializeSelectPic()
 
 })
