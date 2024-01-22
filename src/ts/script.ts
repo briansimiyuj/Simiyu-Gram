281587c4dcd0258f66811083d6d6ffa14b79ab4c
Brian Simiyu <briansimiyuj@gmail.com>
2024-01-22 08:46:02 +0300

    File input functionality

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,6 +1,3 @@
-import initializeSelectPic from './select.js'
-import initializeEditPic from './edit.js'
-
 const desktopUploadLink = document.querySelector("#desktopUploadLink") as HTMLAnchorElement,
       mobileUploadLink = document.querySelector("#mobileUploadLink") as HTMLAnchorElement,
       fileInput = document.querySelector(".fileInput") as HTMLInputElement
@@ -10,16 +7,10 @@ desktopUploadLink.addEventListener("click", () =>{
 
     fileInput.click()
 
-    const selectPic = initializeSelectPic(),
-          editPic = initializeEditPic()
-
 })
 
 mobileUploadLink.addEventListener("click", () =>{
 
     fileInput.click()
-
-    const selectPic = initializeSelectPic(),
-          editPic = initializeEditPic()
 
 })
