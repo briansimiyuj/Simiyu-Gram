437e2ab58f1507f2f1142ce51ef80ce93a72ba25
Brian Simiyu <briansimiyuj@gmail.com>
2024-01-22 17:36:33 +0300

    Select pic functionality

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,8 +1,12 @@
+import initializeSelectPic from './select.js'
+
+
 const uploadLinks = document.querySelectorAll(".uploadLink") as NodeListOf<HTMLAnchorElement>;
 
 uploadLinks.forEach((link) =>{
 
-    const fileInput = document.querySelector(".fileInput") as HTMLInputElement
+    const fileInput = document.querySelector(".fileInput") as HTMLInputElement,
+          selectPic = initializeSelectPic()
 
     link.addEventListener("click", () =>{
     
---
+++ src/ts/select.ts
@@ -0,0 +1,82 @@
+type SelectPicState ={
+    file: File | null
+    error: string | null
+    info: string | null
+
+}
+  
+const initializeSelectPic = () =>{
+
+    const selectPicState: SelectPicState ={
+
+      file: null,
+      error: null,
+      info: null,
+
+    }
+  
+    const types: string[] = ['image/png', 'image/jpeg']
+
+
+  
+    const changeHandler = (event: Event) =>{
+
+      const fileInput = event.target as HTMLInputElement,
+
+            selected = fileInput.files && fileInput.files[0]
+
+  
+      if(selected && types.includes(selected.type)){
+
+        selectPicState.file = selected
+
+        selectPicState.error = null
+
+        selectPicState.info = selected.name
+
+      }else{
+
+        selectPicState.file = null;
+
+        selectPicState.error = 'Please select an image file (png or jpg)'
+
+        selectPicState.info = null
+
+      }
+  
+      updateView()
+
+    }
+  
+    const updateView = () =>{
+
+      const container = document.querySelector(".container") as HTMLDivElement
+
+      const infoContainer = document.createElement("div")
+
+      infoContainer.className = "infoContainer"
+
+      infoContainer.textContent =  selectPicState.error || selectPicState.info
+      
+      container.appendChild(infoContainer)
+
+    }
+  
+    
+    const fileInput = document.querySelector('.fileInput')
+
+    if(fileInput) {
+ 
+        fileInput.addEventListener('input', changeHandler)
+
+    }
+  
+    return{
+
+      getSelectPicState: () => selectPicState
+
+    }
+
+}
+
+export default initializeSelectPic
