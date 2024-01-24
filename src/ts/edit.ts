e21164a6d8ab9925feeefc15e130495cb820f8eb
Brian Simiyu <briansimiyuj@gmail.com>
2024-01-24 13:51:42 +0300

    removeInfoContainer 1

--- src/ts/edit.ts
+++ src/ts/edit.ts
@@ -2,12 +2,33 @@ import initializeSelectPic from './select.js'
 
 const initializeEditPic = () =>{
 
+  removeInfoContainer()
+
+}
+
+
+const removeInfoContainer = () =>{
+
+  const selectPicModule = initializeSelectPic()
+
   document.addEventListener("infoContainerReady", () =>{
   
-      console.log('working')
+    const infoContainer = selectPicModule.getInfoContainer()
+
+    if(infoContainer){
+
+      const leftButton = infoContainer.querySelector("#leftButton") as HTMLButtonElement
+
+      leftButton.addEventListener("click", () =>{
+      
+          console.log('working')
+      
+      })
+
+    }
   
   })
 
-};
+}
 
 export default initializeEditPic 
